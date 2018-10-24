fe4bab4de97c861d6677620d5b35c006095dc3e7
Brenda Wicker <bwicker@byui.edu>
2018-10-24 17:20:05 -0500

    L06 Indiv Assignment. Created GetRandomNumberTests class and added getRandomNumber, setRandomGenerator, and a private Random variable to GameControl.

--- CityOfAaron/src/control/GameControl.java
+++ CityOfAaron/src/control/GameControl.java
@@ -5,15 +5,57 @@
  */
 package control;
 
+import java.util.Random;
+
 /**
  *
  * @author Brenda
  */
 public class GameControl {
     
-    public static int getRandomNumber(int low, int high) {
-        //code to be filled in later
-        return 5;
+    private static Random randomGenerator = new Random();
+    
+    /**
+     * Protected setter for tests to inject a mock random object.
+     * @param random 
+     */
+    protected static void setRandomGenerator(Random random) {
+        randomGenerator = random;
+    }
+    
+    /**
+     * Generates a random integer between min and max, inclusive
+     * <ul>Requirements:
+     * <li>min and max must be positive integers (return -1)</li>
+     * <li>max must be greater than min (return -2)</li>
+     * <li>max cannot be equal to the maximum value for integers (return -3)</li>
+     * </ul>
+     * @param min
+     * @param max
+     * @return The random number
+     */
+    public static int getRandomNumber(int min, int max) {
+        //if min < 0 or max < 0 then return -1
+        if (min < 0 || max < 0) {
+            return -1;
+        }
+        
+        //if max <= min then return -2
+        if (max <= min) {
+            return -2;
+        }
+        
+        //if max is the maximum value for integers, then return -3
+        if (max == Integer.MAX_VALUE) {
+            return -3;
+        }
+        
+        //calculate the size of the range; add one so Random() includes high value
+        int range = (max - min) + 1;
+        
+        //return low + random(range size)
+        return min + randomGenerator.nextInt(range);
+
     }
     
 }
